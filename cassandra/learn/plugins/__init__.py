740cf43565965718297bd134a4ce6bf6d224ee5c
Sarthak-47 <0906sarthak@gmail.com>
2026-05-13 05:55:32 +0530

    feat(sdk): implement client helper __init__ in Python SDK

---
+++ cassandra/learn/plugins/__init__.py
@@ -0,0 +1,5 @@
+"""Built-in learn plugins for cassandra.
+
+Each module in this package exposes a ``plugin`` attribute (a ``LearnPlugin``
+instance) that is auto-discovered by the plugin registry.
+"""
